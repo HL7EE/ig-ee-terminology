d639f75c05ecda74383c3252cc178faee4ca03b3
Igor Bossenko <igor.bossenko@gmail.com>
2024-07-30 22:45:18 +0300

    Fixed deployment url

--- _publish.sh
+++ _publish.sh
@@ -2,7 +2,7 @@
 
 # Read values from YAML file
 version=$(grep '^version:' sushi-config.yaml | awk '{print $2}')
-cmd="./_genonce.sh -publish https://fhir.ee/packages/terminology/$version"
+cmd="./_genonce.sh -publish https://fhir.ee/packages/terminology/$version/site"
 
 # Show version
 echo $cmd
