d0f46b7d4c782288f7e322da4854e9e445cf35c5
Juri Antonov <juri.antonov@kodality.com>
2024-07-30 18:35:34 +0300

    revert build script change

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-publish_url=https://fhir.ee
 
 this="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 if [ ! -f "$this/input-cache/publisher.jar" ]; then
     cd $this && $this/_updatePublisher.sh -y
 fi
 docker run --rm -it -v $this:/workdir -v ~/.fhir:/root/.fhir \
-  docker.kodality.com/fsh-builder /bin/bash -c "cd /workdir && ./_genonce.sh -publish $publish_url"
+  docker.kodality.com/fsh-builder /bin/bash -c "cd /workdir && ./_genonce.sh -publish $1"
