132fafefd7102c4aa52dddaf61924701d51e8ae5
Juri Antonov <juri.antonov@kodality.com>
2025-04-24 16:00:05 +0300

    fix: occupation

--- docker-build.sh
+++ docker-build.sh
@@ -2,8 +2,9 @@
 
 this="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 if [ ! -f "$this/input-cache/publisher.jar" ]; then
-    cd $this && $this/_updatePublisher.sh -y
+    cd $this && $this/_updatePublisher.sh -f -y
 fi
 version=$(grep '^version:' sushi-config.yaml | awk '{print $2}')
-docker run --rm -it -v $this:/workdir -v ~/.fhir:/root/.fhir \
-  docker.kodality.com/fsh-builder /bin/bash -c "cd /workdir && ./_genonce.sh -publish https://fhir.ee/packages/terminology/$version/site"
+docker pull hl7fhir/ig-publisher-base:latest
+docker run --rm -it -v $this:/workdir -v ~/.fhir:/home/publisher/.fhir \
+  hl7fhir/ig-publisher-base:latest /bin/bash -c "cd /workdir && ./_genonce.sh -publish https://fhir.ee/packages/terminology/$version/site"
